56ced6fc511584b2c4eac9fa67c1c3080e347a42
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-25 16:02:23 -0500

    Done
    Exercises 1 and 2 ready

---
+++ src/main/java/Exercise1/Main1.java
@@ -0,0 +1,9 @@
+package Exercise1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        NumberComparison numberComparison = new NumberComparison();
+        numberComparison.isGreater(2,3);
+
+    }
+}
---
+++ src/main/java/Exercise2/Main2.java
@@ -0,0 +1,19 @@
+package Exercise2;
+
+import Exercise1.NumberComparison;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite el primer número");
+        Integer number1 = scan.nextInt();
+        System.out.println("Digite el segundo número");
+        Integer number2 = scan.nextInt();
+
+        NumberComparison numberComparison = new NumberComparison();
+        numberComparison.isGreater(number1,number2);
+    }
+}
